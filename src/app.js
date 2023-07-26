5cdb46a14bcfe6f61b97346313a801ee33b1bf6d
Ivlev Grigoriy <igs94@mail.ru>
2023-07-26 21:46:01 +0300

    feat: initial commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const errorHandler = require('./middlewares/errorHandler');
+const createCustomErrors = require('./middlewares/createCustomErrors');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({ origin: ['http://localhost:3000', 'https://blogogram.nomoreparties.sbs'], credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.use('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use('/', routes);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(createCustomErrors);
+app.use(errorHandler);
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb')
+// eslint-disable-next-line no-console
+  .then(() => console.log('Connected!'));
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening on port ${port}`);
+});

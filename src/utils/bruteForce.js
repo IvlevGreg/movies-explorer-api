7e5c34e6cb3fc58eff759bfa52918b7457ae657f
Ivlev Grigoriy <igs94@mail.ru>
2023-08-01 10:05:17 +0300

    feat: add IS_PRODUCTION

--- src/utils/bruteForce.js
+++ src/utils/bruteForce.js
@@ -2,8 +2,15 @@ const MongooseStore = require('express-brute-mongoose');
 const ExpressBrute = require('express-brute');
 const bruteUsers = require('../models/bruteUsers');
 const failBruteCallback = require('../middlewares/failBruteCallback');
+const { IS_PRODUCTION } = require('./constants/IS_PRODUCTION');
 
-const bruteUsersStore = new MongooseStore(bruteUsers);
+let bruteUsersStore;
+
+if (IS_PRODUCTION) {
+  bruteUsersStore = new MongooseStore(bruteUsers);
+} else {
+  bruteUsersStore = new ExpressBrute.MemoryStore();
+}
 
 const bruteforceAll = new ExpressBrute(bruteUsersStore, {
   freeRetries: 100,
@@ -13,7 +20,7 @@ const bruteforceAll = new ExpressBrute(bruteUsersStore, {
 });
 
 const bruteforceAuth = new ExpressBrute(bruteUsersStore, {
-  freeRetries: 5,
+  freeRetries: IS_PRODUCTION ? 5 : 1000,
   minWait: 5 * 60 * 1000, // 5 minutes
   maxWait: 60 * 60 * 1000, // 1 hour,
   failCallback: failBruteCallback,

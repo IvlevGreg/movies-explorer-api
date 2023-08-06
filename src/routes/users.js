475e385f844bb6d388573546ff442c1797dc7050
Ivlev Grigoriy <igs94@mail.ru>
2023-08-06 22:58:07 +0300

    fix: validatePatchMe

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 const validatePatchMe = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 });
 

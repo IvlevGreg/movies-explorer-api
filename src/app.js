ff3cfe86604c80653a4bea838315e30b7561354b
Ivlev Grigoriy <igs94@mail.ru>
2023-08-30 12:49:46 +0300

    fix: cors url

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors({
   origin: ['http://localhost:3000',
-    'https://blogogram.nomoreparties.sbs'],
+    'http://greg.nomoredomainsicu.ru'],
   credentials: true,
 }));
 

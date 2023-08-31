7646c8c2f35acd2581d801d0e6b517c75fadc5f9
Ivlev Grigoriy <igs94@mail.ru>
2023-08-31 09:45:39 +0300

    Update getValidationErrorText.js

--- src/utils/Errors/getValidationErrorText.js
+++ src/utils/Errors/getValidationErrorText.js
@@ -1,5 +1,7 @@
-export const getValidationErrorText = (errors) => `
+export const getValidationMDErrorText = (errors) => `
 ${Object.values(errors)
     .map((error) => error.message)
     .join(', ')}
     `;
+
+export const getValidationJoiErrorText = (error) => error?.validation?.body?.message;
